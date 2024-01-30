75324bd16373c5e53de9c7cdef1c0ee5b5fa0237
Danny Jaramillo <dajaramillogu@uide.edu.ec>
2024-01-29 21:05:01 -0500

    primer

--- main.py
+++ main.py
@@ -1,30 +1,3 @@
-# from controlador.profesor_controlador import ProfesorControlador
-
-# def menu():
-#     controlador = ProfesorControlador()
-
-#     while True:
-#         print("\n1. Listar profesores")
-#         print("2. Agregar profesor")
-#         print("3. Actualizar profesor")
-#         print("4. Eliminar profesor")
-#         print("5. Salir")
-#         opcion = input("Seleccione una opción: ")
-
-#         if opcion == '1':
-#             controlador.listar_profesores()
-#         elif opcion == '2':
-#             controlador.agregar_profesor()
-#         elif opcion == '3':
-#             controlador.actualizar_profesor()
-#         elif opcion == '4':
-#             controlador.eliminar_profesor()
-#         elif opcion == '5':
-#             break
-
-# if __name__ == "__main__":
-#     menu()
-
 
 
 from controlador.profesor_controlador import ProfesorControlador
